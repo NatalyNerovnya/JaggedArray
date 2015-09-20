19be853cbe80e5df394191bebcd3d7e0462c9666
NatalyNerovnya <natanerovnja@gmail.com>
2015-09-20 17:23:45 +0300

    sorting of jagged array withiout testing sorting according to the max and min elements

---
+++ JaggedArray/JagArr.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    public class JagArr
+    {
+        
+        #region Swap
+        private static void Swap(ref int[] a, ref int[] b)
+        {
+            int[] temp = a;
+            a = b;
+            b = temp;
+        }
+        #endregion 
+
+        #region Sorting according to the sum
+        public static void SortSum(ref int[][] arr)
+        {
+            if (arr == null)
+                throw new ArgumentNullException();
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i].Sum() < arr[j].Sum())
+                    {
+                        Swap(ref arr[i], ref arr[j]);
+                    }
+                }
+        }
+        #endregion
+
+        #region Sorting according to the max element in the row
+        public static void SortMax(ref int[][] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i].Max() < arr[j].Max())
+                    {
+                        Swap(ref arr[i], ref arr[j]);
+                    }
+                }
+        }
+        #endregion
+
+        #region Sorting according to the min element in the row
+        public static void SortMin(ref int[][] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i].Min() < arr[j].Min())
+                    {
+                        Swap(ref arr[i], ref arr[j]);
+                    }
+                }
+        }
+        #endregion
+    }
+}
---
+++ JaggedArrayTests/JagArrTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JaggedArray;
+
+namespace JaggedArrayTests
+{
+    [TestClass]
+    public class JagArrTests
+    {
+        [TestMethod]
+        public void SortSum_PositiveArray_SortedArrayAccordingToTheSum()
+        {
+            int row = 3;
+            int[][] jagArr = new int[row][];
+            jagArr[0] = new int[] { 6, 3, 2 };
+            jagArr[1] = new int[] { 3, 5, 2, 0, 6, 1 };
+            jagArr[2] = new int[] {6, 3, 4};
+
+            int[][] resultArray = new int[row][];
+            resultArray[0] = new int[] { 3, 5, 2, 0, 6, 1 };
+            resultArray[1] = new int[] { 6, 3, 4 };
+            resultArray[2] = new int[] { 6, 3, 2 };
+
+            JagArr.SortSum(ref jagArr);
+
+            CollectionAssert.AreEqual(jagArr[0], resultArray[0]);
+            CollectionAssert.AreEqual(jagArr[1], resultArray[1]);
+            CollectionAssert.AreEqual(jagArr[2], resultArray[2]);
+        }
+
+        [TestMethod]
+        public void SortSum_NegativeArray_SortedArrayAccordingToTheSum()
+        {
+            int row = 3;
+            int[][] jagArr = new int[row][];
+            jagArr[0] = new int[] { -6, -3, -2 };
+            jagArr[1] = new int[] { -3, -5, -2, -6, -1 };
+            jagArr[2] = new int[] { -6, -3, -4 };
+            
+            int[][] resultArray = new int[row][];
+            resultArray[0] = new int[] { -6, -3, -2 };
+            resultArray[1] = new int[] { -6, -3, -4 };
+            resultArray[2] = new int[] { -3, -5, -2, -6, -1 };
+
+            JagArr.SortSum(ref jagArr);
+
+            CollectionAssert.AreEqual(jagArr[0], resultArray[0]);
+            CollectionAssert.AreEqual(jagArr[1], resultArray[1]);
+            CollectionAssert.AreEqual(jagArr[2], resultArray[2]);
+        }
+
+        [TestMethod]
+        public void SortSum_DifferentElementsArray_SortedArrayAccordingToTheSum()
+        {
+            int row = 3;
+            int[][] jagArr = new int[row][];
+            jagArr[0] = new int[] { -6, 3, -2 };
+            jagArr[1] = new int[] { -3, -5, 2, -6, 1 };
+            jagArr[2] = new int[] { 6, -3, 4 };
+
+            int[][] resultArray = new int[row][];
+            resultArray[0] = new int[] { 6, -3, 4 };//7
+            resultArray[1] = new int[] { -6, 3, -2 };//-5
+            resultArray[2] = new int[] { -3, -5, 2, -6, 1 };//-11
+
+            JagArr.SortSum(ref jagArr);
+
+            CollectionAssert.AreEqual(jagArr[0], resultArray[0]);
+            CollectionAssert.AreEqual(jagArr[1], resultArray[1]);
+            CollectionAssert.AreEqual(jagArr[2], resultArray[2]);
+        }
+
+    }
+}
